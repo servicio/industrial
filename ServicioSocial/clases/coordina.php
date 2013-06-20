1114ad1dcda910191c723f5c8c955776e11c87ce
Solis <Solis@Solis-PC>
2013-06-20 17:54:39 -0500

    prr datos prr

---
+++ ServicioSocial/clases/coordina.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of coordina
+ *
+ * @author Solis
+ */
+class coordina {
+
+    private $niombre;
+
+    public function getNiombre() {
+        return $this->niombre;
+    }
+
+    public function setNiombre($niombre) {
+        $this->niombre = $niombre;
+    }
+}
+
+?>
