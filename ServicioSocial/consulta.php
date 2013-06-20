f9bcd21bdf85accaa8e3628ad6b56ca29a397fbf
Comodoro <Comodoro@pjgt>
2013-06-20 15:27:14 -0500

    asd

---
+++ ServicioSocial/consulta.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/css.css">
+        <link rel="stylesheet" type="text/css" href="bootsTrap/css/bootstrap.css"/>
+        <script src="bootsTrap/js/jquery.min.js"></script>
+        <script src="bootsTrap/js/bootstrap.js"></script>
+    </head>
+    <body>
+    <center>
+        <div  style="width: 960px"class="hero-unit">
+            <h2 style="margin-right: 600px">
+                Captura de Materias
+            </h2>
+            <hr>
+            <input style="margin-right:680px" type="text" name="matricula" placeholder="Matricula..."/>
+            <br>
+            <select name="materias" style="margin-right:680px"id="materias">
+                <option>Seleccione Matriculas</option>
+            </select>
+            <br>
+
+            Nombre:Pablo Javier Gonzalez Torres.
+            <br>
+            Semestre: 10
+            <br>
+            Curso escolas: Enero-Junio
+            <table class="table table-hover">
+                <thead>
+                <td>Materias</td>
+                <td>Materias</td>
+                <td>Materias</td>
+                <td>Materias</td>
+                <td>Materias</td>
+                </thead>
+                <tr>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                </tr>
+                <tr>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                </tr>
+                <tr>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                </tr>
+                <tr>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                    <td>Hola</td>
+                </tr>
+            </table>
+        </div>
+    </center>
+</body>
+</html>
+
---
+++ ServicioSocial/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/css.css">
+        <link rel="stylesheet" type="text/css" href="bootsTrap/css/bootstrap.css"/>
+        <script src="bootsTrap/js/jquery.min.js"></script>
+        <script src="bootsTrap/js/bootstrap.js"></script>
+    </head>
+    <body>
+    <center>
+        <br><br><br><br><br><br><br>
+        <div class="hero-unit">
+
+            <h1 style="color:#56ae56 "> Inicio de Sesión</h1>
+            <br>
+            <input type="text" placeholder="Usuario">
+            <br>
+            <input  type="text" placeholder="Contraseña">
+            <br>
+            <button type="submit" class="btn btn-primary"> <i class="icon-ok">&nbsp;</i>&nbsp;&nbsp;Iniciar</button>
+            <div style="width: 960px" class="alert-error">
+                Ingrese el <Strong>Usuario y Contraseña</Strong> correcta
+            </div>
+        </div>
+    </center>
+</body>
+</html>
