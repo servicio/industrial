3194741b2340d41b5e685ddf2d33c20f107ae321
Comodoro <Comodoro@pjgt>
2013-06-20 18:26:45 -0500

    ClaseConeccion

---
+++ ServicioSocial/DaoConnection/coneccion.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of coneccion
+ *
+ * @author Comodoro
+ */
+class coneccion {
+
+    function conectarse() {
+        if (!($link = mysql_connect("localhost", "root", ""))) {
+//    if (!($link = mysql_connect("mysql.pcoriente.com.mx", "pablo_torres", "Pato82"))) {
+            echo "Error conectando a la base de datos.";
+            exit();
+        }
+        if (!mysql_select_db("10d2", $link)) {
+            echo "Error seleccionando la base de datos.";
+            exit();
+        }
+        return $link;
+    }
+
+}
+
+?>
