d945c2fdb65ad100f5d600b3d8fde6f6762439eb
Comodoro <Comodoro@pjgt>
2013-07-03 19:00:16 -0500

    Activacion de camara Finalizada

---
+++ ServicioSocial/alumnos/cargaArchivos.php
@@ -0,0 +1,24 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+<script type="text/javascript" src="../bootsTrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="../bootsTrap/js/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../bootsTrap/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="../bootsTrap/css/bootstrap-responsive.css"/>
+<link rel="stylesheet" type="text/css" href="../bootsTrap/css/bootstrap-responsive.min.css"/>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css"/>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.min.css"/>
+<link rel="stylesheet" type="text/css" href="../bootsTrap/css/bootstrap.min.css"/>
+<center>
+    <div id="cel" class="container" style="background-color: #f6f3f2">
+        <div class="row">
+            <div class="span12" >
+                <?php include './menu.php'; ?>
+
+                <br/>
+                <form>
+                    
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</center>
---
+++ ServicioSocial/alumnos/foto.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" type="text/css" href="jquery.lightbox-0.5.css" media="screen" />
+<script type="text/javascript" src="webcam.js"></script>
+<script language="JavaScript">
+    webcam.set_api_url('test.php');//PHP adonde va a recibir la imagen y la va a guardar en el servidor
+    webcam.set_quality(90); // calidad de la imagen
+    webcam.set_shutter_sound(true); // Sonido de flash
+</script>
+<script language="JavaScript">
+    webcam.set_hook('onComplete', 'my_completion_handler');
+
+    function do_upload() {
+        // subir al servidor
+        document.getElementById('upload_results').innerHTML = '<h1>Cargando al servidor...</h1>';
+        webcam.upload();
+    }
+
+    function my_completion_handler(msg) {
+
+        if (msg.match(/(http\:\/\/\S+)/)) {
+            var image_url = RegExp.$1;//respuesta de text.php que contiene la direccion url de la imagen
+            // Muestra la imagen en la pantalla
+            document.getElementById('upload_results').innerHTML =
+                    '<img src="' + image_url + '">';
+//                    '<form action="gestion_foto.php" method="post">' +
+//                    '<input type="hidden" name="id_foto" id="id_foto" value="' + image_url + '"  /><br>' +
+//                    '<label>Nombre </label><input type="text" name="nombre_foto" id="nombre_foto"/>' +
+//                    '<label>Descripcion </label><input type="text" name="des" id="des"/>' +
+//                    '<input type="submit" name="button" id="button" value="Enviar" /></form>'
+//                    ;
+                    // reset camera for another shot
+                    webcam.reset();
+
+        }
+        else
+            alert("PHP Error: " + msg);
+    }
+</script>
+<div align="left" id="cuadro_camara">    
+
+    <form>
+<!--        <input type=button value="Configurar" onClick="webcam.configure()" class="btn btn-danger">
+        &nbsp;&nbsp;-->
+        <input type=button value="Tomar foto" onClick="webcam.freeze()" class="btn btn-info">
+        &nbsp;&nbsp;
+        <input type=button value="Guardar " onClick="do_upload()"  name="dato" class="btn btn-success"/>
+
+
+        &nbsp;&nbsp;
+        <input type=button value="Cancelar" onClick="webcam.reset()" class="btn btn-warning">
+    </form>
+    <div id="visualizacionCamara">
+        <script language="JavaScript">
+    document.write(webcam.get_html(250, 280));//dimensiones de la camara
+        </script>
+    </div>
+    <div style="float: right" id="upload_results" class="formulario" > </div>
+    <br />
+</div>
