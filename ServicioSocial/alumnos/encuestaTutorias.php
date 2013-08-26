f880f63482c9003471c971e8aab8e6609e123182
Joel <Joel@JACA>
2013-08-26 10:38:30 -0500

    Agregados acentos y simbolos en encuestaTutorias FINAL

--- ServicioSocial/alumnos/encuestaTutorias.php
+++ ServicioSocial/alumnos/encuestaTutorias.php
@@ -144,7 +144,7 @@
         10.- &iquest;Que materias te gustan m&aacute;s?<br>
         <input type="text" name="HabMaterias"><br>
 
-        11.- &iquest;Realizas otros estudios fuera de tecnológico?<br>
+        11.- &iquest;Realizas otros estudios fuera de tecnol&oacute;gico?<br>
         <input type="radio" name="EstudiosExtTec" value="Si" id="RadioGroup10_0" onClick="opcion10.disabled = false">
         Si (especifica)
         <!--Cuadro de texto-->
@@ -181,53 +181,53 @@
         No<br>
         13.1.- &iquest;En que trabajas?<br>
         <input name="opcion12y1" type="text" disabled="disabled"><br>
-        13.2.- &iquest;cual es el motivo por el cual laboras?<br>
+        13.2.- &iquest;Cu&aacute;l es el motivo por el cual laboras?<br>
         <input name="opcion12y2" type="text" disabled="disabled"><br>
-        13.3.- &iquest;Ocupacion en el trabajo?<br>
+        13.3.- &iquest;Ocupaci&oacute;n en el trabajo?<br>
         <input name="opcion12y3" type="text" disabled="disabled"><br>
 
 
-        14.- &iquest;Cuales son las razones por la que ingrsaste al tecnologico?
+        14.- &iquest;Cu&aacute;l es la razon por la que ingresaste al tecnol&oacute;gico?
         <br><input type="radio" name="IngresastRazTec" value="opción1" id="RadioGroup12_0" onClick="opcion13.disabled = true">
         Porque esta es la escuela que desean mis padres.<br>
         <input type="radio" name="IngresastRazTec" value="opción2" id="RadioGroup12_1" onclick="opcion13.disabled = true">
         porque no es cara la colegiatura.<br>
         <input type="radio" name="IngresastRazTec" value="opción3" id="RadioGroup12_2" onclick="opcion13.disabled = true">
-        Porque la institucion tiene prestigio<br>
+        Porque la instituci&oacute;n tiene prestigio<br>
         <input type="radio" name="IngresastRazTec" value="opción4" id="RadioGroup12_3" onclick="opcion13.disabled = true">
         porque tiene buenos egresados. <br>
         <input type="radio" name="IngresastRazTec" value="opción4" id="RadioGroup12_4" onclick="opcion13.disabled = true">
-        Porque no alcanze cupo en otra escuela. <br>
+        Porque no alcance cupo en otra escuela. <br>
         <input type="radio" name="IngresastRazTec" value="opción5" id="RadioGroup12_5" onclick="opcion13.disabled = false">
         Otro(Especifica) 
         <input name="opcion13" type="text" disabled="disabled"><br>
 
-        15.- &iquest;Cuales son las razones por la que ingresaste a la carrera?<br>
+        15.- &iquest;Cu&aacute;les son las razones por la que ingresaste a la carrera?<br>
         <input type="radio" name="IngresastRazCar" value="opción2" id="RadioGroup13_0" onclick="opcion14.disabled = true">
-        porque espero ganar mucho dinero con ella.<br> 
+        Porque espero ganar mucho dinero con ella.<br> 
         <input type="radio" name="IngresastRazCar" value="opción3" id="RadioGroup13_1" onclick="opcion14.disabled = true">
         Porque les la carrera que he elegido y me apasiona<br>
         <input type="radio" name="IngresastRazCar" value="opción4" id="RadioGroup13_2" onclick="opcion14.disabled = true">
-        por su plan de estudios. <br>
+        Por su plan de estudios. <br>
         <input type="radio" name="IngresastRazCar" value="opción4" id="RadioGroup13_3" onclick="opcion14.disabled = true">
         Porque no hubo cupo en la carrera que me interesa. <br>
         <input type="radio" name="IngresastRazCar" value="opción4" id="RadioGroup13_4" onclick="opcion14.disabled = true">
-        Porque es la carrera que desena mis padres.<br> 
+        Porque es la carrera que desean mis padres.<br> 
         <input type="radio" name="IngresastRazCar" value="opción1" id="RadioGroup13_5" onClick="opcion14.disabled = true">
-        Porque es la unica escuela que da esta carrera.<br>
+        Porque es la &uacute;nica escuela que da esta carrera.<br>
         <input type="radio" name="IngresastRazCar" value="opción5" id="RadioGroup13_6" onclick="opcion14.disabled = false">
-        Otro(Especifica) 
+        Otro (Especifica) 
         <input name="opcion14" type="text" disabled="disabled">  <br>
 
         16.- &iquest;Padece alguna alergia?<br>
         <input type="radio" name="Alergias" value="opción1" id="RadioGroup14_0" onClick="CualAlergia.disabled = false">
         Si
-        ¿cual?<input name="CualAlergia" type="text" disabled="disabled">
+        ¿cu&aacute;l?<input name="CualAlergia" type="text" disabled="disabled">
         <input type="radio" name="Alergias" value="opción2" id="RadioGroup14_1" onclick="CualAlergia.disabled = true">
         No
         <br>
 
-        17.- &iquest;Padece alguna enfermedad cronica?<br>
+        17.- &iquest;Padece alguna enfermedad cr&oacute;nica?<br>
         <input type="radio" name="Cronica" value="opción1" id="RadioGroup15_0" onClick="CronicaCual.disabled = false">
         Si
         ¿Cuál?<input name="CronicaCual" type="text" disabled="disabled">
@@ -235,7 +235,7 @@
         No
         <br>
 
-        18.- &iquest;Has recibido atencion psicologica?<br>
+        18.- &iquest;Has recibido atenci&oacute;n psicol&oacute;gica?<br>
         <input type="radio" name="AtencionPsi" value="opción1" id="RadioGroup16_0" onClick="CualAtencionPsi.disabled = false">
         Si
         (especifica de que tipo)<input name="CualAtencionPsi" type="text" disabled="disabled">
@@ -243,7 +243,7 @@
         No 
         <br>
 
-        19.- &iquest;Donde recibes atencion medica?<br>
+        19.- &iquest;D&oacute;nde recibes atenci&oacute;n medica?<br>
         <input type="radio" name="AtencionMedica" value="opción1" id="RadioGroup17_0">
         IMSS
         <input type="radio" name="AtencionMedica" value="opción2" id="RadioGroup17_1">
@@ -275,13 +275,13 @@
         No 
         <br>
 
-        23.- &iquest;Practicas algun deporte?
+        23.- &iquest;Practicas al&uacute;n deporte?
         <input type="radio" name="Deporte" value="opción1" id="RadioGroup21_0" onClick="CualDep.disabled = false, FrecuenciaDept.disabled = false">
         Si
         <input type="radio" name="Deporte" value="opción2" id="RadioGroup21_1" onclick="CualDep.disabled = true, FrecuenciaDept.disabled = true">
         No <br>
-        23.1.- &iquest;Cual? <input name="CualDep" type="text" disabled="disabled">  <br>
-        23.2.- &iquest;Conque frecuencia lo realizas?<input name="FrecuenciaDept" type="text" disabled="disabled"> <br>
+        23.1.- &iquest;Cu&aacute;l? <input name="CualDep" type="text" disabled="disabled">  <br>
+        23.2.- &iquest;Con que frecuencia lo realizas?<input name="FrecuenciaDept" type="text" disabled="disabled"> <br>
 
         <input type="submit" name="guardar" value="Guardar" >
     </form>
