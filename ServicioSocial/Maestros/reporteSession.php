cd274005d39c3e504eaf300f708d5caef61e5664
Comodoro <Comodoro@pjgt>
2013-08-27 12:51:52 -0500

    Resuelto Checar Sentencia de Sql

---
+++ ServicioSocial/Maestros/reporteSession.php
@@ -0,0 +1,18 @@
+<?php
+include './plantilla.php';
+?> 
+<div class="container">
+    <div class="span12"  style="margin: auto">
+        <br>
+        <div style="float: left">
+            <select>
+                <option>Seleccione un Usuario</option>
+            </select>
+        </div>
+        <div style="float: right">N°. Session</div>
+
+        <br>
+        <?php  date(dd / mm / yyyy); ?>
+
+    </div>
+</div>
