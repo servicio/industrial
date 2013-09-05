a8c814a55615b3c5c3bbfe8e53beec018465eeed
Joel <Joel@JACA>
2013-09-05 17:02:33 -0500

    Archivo cargar archivos editado Se agrego un JS para la carga de archivos

--- ServicioSocial/alumnos/cargaArchivos.php
+++ ServicioSocial/alumnos/cargaArchivos.php
@@ -1,24 +1,52 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
-<script type="text/javascript" src="../bootsTrap2/js/bootstrap.js"></script>
-<script type="text/javascript" src="../bootsTrap2/js/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap.css"/>
-<link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap-responsive.css"/>
-<link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap-responsive.min.css"/>
-<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css"/>
-<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.min.css"/>
-<link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap.min.css"/>
-<center>
-    <div id="cel" class="container" style="background-color: #f6f3f2">
-        <div class="row">
-            <div class="span12" >
-                <?php include './menu.php'; ?>
+<?php
+include './plantilla.php';
+?>
+<html>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/css.css">
+    <link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap-responsive.css"/>
 
-                <br/>
-                <form>
-                    
-                    
-                </form>
-            </div>
+    <script src="../bootsTrap2/js/jquery.min.js"></script>
+    <script src="../bootsTrap2/js/bootstrap.js"></script>
+    <script src="../bootsTrap2/js/bootstrap.file-input.js"></script>
+    <script>$(document).ready(function() {
+            $('input[type=file]').bootstrapFileInput();
+        });</script>
+
+    <div class="container" style="background-image: url(galeria/fondotextofinal.png);-webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; margin-top: -20px">
+        <div class="span12" style="margin: auto">
+            <form name="subearchivos" action="" style="margin-left: 50px; margin-top: 50px">
+                <h3>Sube tus documentos...</h3>
+                <table>
+                    <tbody>
+                        <!------------------------------------------------------------------------------------------------>
+                        <tr>
+                            <td class="formulario">
+                                <h5>Primer documento:</h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="btn-group">
+                                <input type="file" name="" id="" accept="application/pdf" title="Buscar Archivo">
+                                <input type="submit" class="btn btn-primary" name="" value="Enviar" id="">
+                            </td>
+                        </tr>
+                        <!------------------------------------------------------------------------------------------------>
+                        <tr>
+                            <td class="formulario">
+                                <h5>Segundo documento:</h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="btn-group">
+                                <input type="file" name="" id="" accept="application/pdf" title="Buscar Archivo">
+                                <input type="submit" class="btn btn-primary" name="" value="Enviar" id="">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
         </div>
     </div>
-</center>
+</html>
