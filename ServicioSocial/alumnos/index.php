850e0160c2844f90852342505ef1a9834b7db85c
Joel <Joel@JACA>
2013-09-15 12:13:01 -0500

    Mmmm...

--- ServicioSocial/alumnos/index.php
+++ ServicioSocial/alumnos/index.php
@@ -2,12 +2,6 @@
 include './plantilla.php';
 ?>
 <html>
-    <link rel="stylesheet" type="text/css" href="../css/css.css">
-    <link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap.css"/>
-    <link rel="stylesheet" type="text/css" href="../bootsTrap2/css/bootstrap-responsive.css"/>
-
-    <script src="../bootsTrap2/js/jquery.min.js"></script>
-    <script src="../bootsTrap2/js/bootstrap.js"></script>
 </html>
 <?php
 include './plantillaFooter.php';
