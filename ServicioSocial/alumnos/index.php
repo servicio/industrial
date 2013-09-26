cbc3ae3a02f8689fa123d042e69877ab4c9a633c
Administrador <Administrador@ba-556e4d6b45a7>
2013-09-26 14:32:46 -0500

    ultimaActualizacionjose

--- ServicioSocial/alumnos/index.php
+++ ServicioSocial/alumnos/index.php
@@ -1,45 +1,47 @@
 <?php
 include './plantilla.php';
-
+session_start();
 include '../DaoConnection/coneccion.php';
 
 $cn = new coneccion();
-$sql = "SELECT * FROM avisostutor ORDER BY id DESC LIMIT 20";
+$sql = "select * from  tutotmaestrosalumnos t , maestros m , avisostutor  av where matricula ='e09080264' 
+    and t.idMaestro = m.id and m.usuario = av.usuario and av.control = 3";
+$usuario=$_SESSION["Usuario"];
 $datos = mysql_query($sql, $cn->Conectarse());
 $cn->cerrarBd();
 
 ?>
 
 <div class="container">
+
+    <center>
+        <div    class="span12"  style="overflow-y: scroll; height: 335px;  margin: auto; background-color: white; margin-top: -20px">
+            <?php
             
-            <center>
-                <div    class="span12"  style="overflow-y: scroll; height: 335px;  margin: auto; background-color: white; margin-top: -20px">
-                    <?php
-                    //echo '<table>';
-                    while ($row = mysql_fetch_array($datos)) {
-                        echo "<div style='float:left ; margin-left: 100px'>
+            while ($row = mysql_fetch_array($datos)) {
+                echo "<div style='float:left ; margin-left: 100px'>
                             <h3>"
-                        . $row["titulo"] .
-                        "</h3>
+                . $row["titulo"] .
+                "</h3>
                                 </div>
                                 <br> <br>
                             ";
 
-               echo "<center>
+                echo "<center>
                             <div style='width:60%'>
                                 <p align='justify'>"
-                                    . $row["detalles"] . "
+                . $row["detalles"] . "
                                 </p>
                             </div>
                      </center>
                               <br/>                                
                               ";
-                    }
-                    ?>
+            }
+            ?>
 
-                </div>
-            </center>
         </div>
+    </center>
+</div>
 
 <?php
 include './plantillaFooter.php';
