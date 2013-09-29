ee0c939ad358a440cc25569db942d13e6683e9ac
Administrador <Administrador@ba-556e4d6b45a7>
2013-09-29 13:27:59 -0500

    a

--- ServicioSocial/Coordinadora/autorizacionCorreos.php
+++ ServicioSocial/Coordinadora/autorizacionCorreos.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 include '../DaoConnection/coneccion.php';
+include '../Dao/daoServicio.php';
+
+include '../alumnos/plantilla.php';
+include '../alumnos/plantillaFooter.php';
+
 
 
 ?>
